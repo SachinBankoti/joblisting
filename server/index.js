93158102a799c4a1a6025c0006543fb7611c21a7
Sachin Bankoti <sachinbankoti16@gmail.com>
2023-10-13 10:47:54 -0400

    backend git setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-console.log('server');
+import express from "express"
+
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT,console.log(`Server is running in PORT${PORT}`))
